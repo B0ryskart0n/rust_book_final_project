3b8f743fbbc922fc3ecddf40a72f178dfd7bb3f3
Borys Kopeć <boryskopec00@gmail.com>
2026-05-30 22:37:28 +0200

    Handle connections with hardcoded 200 response

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
-use std::net::TcpListener;
+use std::{
+    io::BufReader,
+    io::prelude::*,
+    net::{TcpListener, TcpStream},
+};
 
 const ADDR: &str = "127.0.0.1:7878";
 
 fn main() {
-    let listener = TcpListener::bind(ADDR).expect("Couldn't bind the listener");
+    let listener = TcpListener::bind(ADDR).expect("couldn't bind the listener");
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        // a single `stream` represents an open connection between the client and the server.
+        // A single `stream` represents an open connection between the client and the server.
 
-        println!("Connection established!");
+        handle_connection(stream);
         // `stream` gets dropped and the connection is closed.
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|line_result| line_result.expect("request line couldn't be read."))
+        .take_while(|line| !line.is_empty()) // Two newline characters signal end of HTTP request.
+        .collect();
+
+    eprintln!("Request: {http_request:#?}");
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream
+        .write_all(response.as_bytes())
+        .expect("couldn't write a response to stream");
 }
