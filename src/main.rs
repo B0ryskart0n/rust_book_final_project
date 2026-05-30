c4cf16e0d9a59b9d82b62b8cde0f8bfd8d9a2a1b
Borys Kopeć <boryskopec00@gmail.com>
2026-05-30 22:05:08 +0200

    Dummy TCP listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::net::TcpListener;
+
+const ADDR: &str = "127.0.0.1:7878";
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind(ADDR).expect("Couldn't bind the listener");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        // a single `stream` represents an open connection between the client and the server.
+
+        println!("Connection established!");
+        // `stream` gets dropped and the connection is closed.
+    }
 }
