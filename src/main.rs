4cee6dcabf66bd612bc329585ad301d50dc61850
Borys Kopeć <boryskopec00@gmail.com>
2026-05-31 12:08:00 +0200

    Fix problem with empty stream, further logging

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::{
     fs,
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 const ADDR: &str = "127.0.0.1:7878";
@@ -29,13 +31,20 @@ fn handle_connection(mut stream: TcpStream) {
         .map(|line_result| line_result.expect("request line couldn't be read as utf8"))
         .take_while(|line| !line.is_empty()) // Two newline characters signal end of HTTP request. Without this condition the iterator will not finish because the stream is open and the sender could send more stuff.
         .collect::<Vec<String>>();
-    eprintln!("Request lines: {request_lines:?}");
+    eprintln!("Request lines: {request_lines:?}.");
 
-    // TODO Can panic if the request is empty.
-    // It seems like the browser tries to always maintain a connection, but doesn't send anything if it already has the content, so closing the browser crashes this program.
-    let (status, filename) = match &request_lines[0][..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    // It seems like the browser tries to always maintain a connection, but doesn't send anything if it already has the content, so closing the browser gives an empty request.
+    let (status, filename) = match request_lines.get(0).map(|string| &string[..]) {
+        Some("GET / HTTP/1.1") => ("HTTP/1.1 200 OK", "hello.html"),
+        Some("GET /sleep HTTP/1.1") => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        Some(_) => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+        None => {
+            eprintln!("Empty stream closed: returning.");
+            return;
+        }
     };
 
     let contents = fs::read_to_string(filename).expect("couldn't read html file");
@@ -47,5 +56,5 @@ fn handle_connection(mut stream: TcpStream) {
     stream
         .write_all(response.as_bytes())
         .expect("couldn't write a response to stream");
-    eprintln!("Sent resposne");
+    eprintln!("Sent resposne.");
 }
