2971bd13e8874040d8c33412714c4e4cd0cb1bf3
Borys Kopeć <boryskopec00@gmail.com>
2026-06-02 20:41:03 +0200

    Handle main dropping the threadpool gracefully

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,13 @@ use std::thread;
 
 pub struct ThreadPool {
     _workers: Vec<Worker>,
-    sender: Sender<Box<dyn FnOnce() + Send + 'static>>,
+    sender: Option<Sender<Box<dyn FnOnce() + Send + 'static>>>,
 }
 impl ThreadPool {
     pub fn new(n: usize) -> ThreadPool {
         let (sender, receiver) = mpsc::channel();
         let shared_receiver = Arc::new(Mutex::new(receiver));
+        let sender = Some(sender);
 
         // Could potentially create 0 workers, but since it's only called from my binary.
         let workers = (0..n)
@@ -24,8 +25,22 @@ impl ThreadPool {
 
     pub fn execute<F: FnOnce() + Send + 'static>(&self, f: F) {
         self.sender
+            .as_ref()
+            .unwrap()
             .send(Box::new(f))
             .expect("channel in an ill state, can't send closure");
+    }
+}
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        // Move out the underlying `sender` and drop it. This will put the channel in an ill state.
+        drop(self.sender.take());
+
+        // `Vec::drain` moves the workers out of the vector, which is needed for `JoinHandle::join()`.
+        for worker in self._workers.drain(..) {
+            eprintln!("Shutting down worker {}.", worker._id);
+            worker._thread.join().unwrap();
+        }
     }
 }
 
@@ -42,9 +57,16 @@ impl Worker {
                 let receiver_guard = receiver.lock().expect("mutex in an ill state, can't lock");
                 eprintln!("    Worker {_id} acquired lock for job receiver.");
 
-                let job = receiver_guard
-                    .recv()
-                    .expect("channel in an ill state, can't query receiver");
+                let job = match receiver_guard.recv() {
+                    Err(_) => {
+                        eprintln!(
+                            "    Worker {_id} shutting down because the sender has been disconnected."
+                        );
+                        break;
+                    }
+                    Ok(job) => job,
+                };
+
                 // Free the lock before proceeding with the job. Otherwise there will be no concurrency because no other worker will be able to read the job receiver during this worker's work.
                 drop(receiver_guard);
 
